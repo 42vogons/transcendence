94ee4a9fc0f52f212692570272afe0c136bf971d
cpereira42 <cpereira@student.42sp.org.br>
2023-12-02 23:52:58 -0300

    feat: initial config for nestjs

---
+++ back/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GreetingController } from './greeting/greeting.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, GreetingController],
+  providers: [AppService],
+})
+export class AppModule {}
