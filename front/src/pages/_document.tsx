f688e1426335277cd896e69a44e37d57ed936f99
a-cs <acarneirosousa@gmail.com>
2023-12-04 23:04:41 -0300

    feat: create login page

--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -1,13 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@900&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
