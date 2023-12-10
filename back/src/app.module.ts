e3e29b45f37e98607dcc984a09781b163847bf70
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2023-12-10 19:47:07 -0300

    feat: import config from project root and lint configs

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controller/app.controller'
+import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
 import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
