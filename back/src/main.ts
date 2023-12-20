0b54c4e431072cb930f1562685c0c409f5f85ff4
anolivei <anolivei@student.42sp.org.br>
2023-12-20 01:15:40 -0300

    feat: unauthorized interceptor

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,7 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
   });
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
   await app.listen(process.env.BACK_PORT || 3001);
 }
 bootstrap();
