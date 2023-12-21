a45014ee3d9da1e2fcaf28e3da4c0870c677fba3
anolivei <anolivei@student.42sp.org.br>
2023-12-20 23:24:22 -0300

    feat: not found error when user does not exists in find one

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,6 +4,7 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,7 @@ async function bootstrap() {
   });
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
   await app.listen(process.env.BACK_PORT || 3001);
 }
 bootstrap();
