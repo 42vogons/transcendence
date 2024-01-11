ae586de08599fa028f2efde1a6ecd09ca9ce60ba
a-cs <acarneirosousa@gmail.com>
2024-01-10 23:44:16 -0300

    feat: chat list

---
+++ front/src/styles/components/chatListItem.ts
@@ -0,0 +1,78 @@
+import { styled } from '..'
+
+export const ChatListItemContainer = styled('button', {
+	width: '100%',
+	display: 'flex',
+	flexDirection: 'row',
+	justifyContent: 'space-between',
+	alignItems: 'center',
+	gap: 8,
+	color: '$white',
+	backgroundColor: '$blue600',
+	border: 'none',
+	borderBottom: '1px solid $white',
+	padding: '0.5rem 1rem 0.5rem 0.5rem',
+	cursor: 'pointer',
+
+	'&:hover': {
+		backgroundColor: '$blue300',
+	},
+
+	variants: {
+		isActive: {
+			true: {
+				backgroundColor: '$blue200',
+			},
+		},
+	},
+})
+
+export const ChatAvatarContainer = styled('div', {
+	position: 'relative',
+	borderRadius: '50%',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+
+	img: {
+		borderRadius: '50%',
+		border: '2px $blue600 solid',
+	},
+})
+
+export const ChatInfoContainer = styled('div', {
+	height: '100%',
+	minWidth: 0,
+	width: '100%',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'space-between',
+	alignItems: 'start',
+	gap: 8,
+})
+
+export const ChatName = styled('h2', {
+	fontSize: '$md',
+	fontFamily: 'monospace',
+	fontVariantNnumeric: 'slashed-zero',
+})
+
+export const ChatLastMessage = styled('p', {
+	width: '100%',
+	color: '$gray100',
+	fontSize: '1rem',
+	textAlign: 'start',
+	opacity: '70%',
+	whiteSpace: 'nowrap',
+	overflow: 'hidden',
+	textOverflow: 'ellipsis',
+
+	variants: {
+		isActive: {
+			true: {
+				color: '$white',
+				opacity: '100%',
+			},
+		},
+	},
+})
