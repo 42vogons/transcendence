8232bd6babc25d61ded7a96764d1726e2bb7a911
anolivei <anolivei@student.42sp.org.br>
2024-01-18 00:07:35 -0300

    feat: initial config to websockets

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,10 +3,11 @@ import { LoginController } from './login/login.controller';
 import { LoginService } from './login/login.service';
 import { LoginModule } from './login/login.module';
 import { UsersModule } from './users/users.module';
+import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
   imports: [LoginModule, UsersModule],
   controllers: [LoginController],
-  providers: [LoginService],
+  providers: [LoginService, ChatGateway],
 })
 export class AppModule {}
