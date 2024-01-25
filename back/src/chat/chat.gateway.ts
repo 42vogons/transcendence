cca1c148f8493237a9b2e30baacf23ca549d56b3
anolivei <anolivei@student.42sp.org.br>
2024-01-25 00:39:23 -0300

    feat: chat dto and save message on prisma

--- back/src/chat/chat.gateway.ts
+++ back/src/chat/chat.gateway.ts
@@ -8,6 +8,8 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ChatDto } from './dto/chat.dto';
 
 @WebSocketGateway()
 export class ChatGateway
@@ -16,20 +18,35 @@ export class ChatGateway
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('AppGateway');
 
+  constructor(private readonly prisma: PrismaService) {}
+
   @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, payload: string): void {
-    this.server.emit('msgToClient', payload, client.id);
+  async handleMessage(client: Socket, payload: ChatDto): Promise<void> {
+    try {
+      const newMessage = await this.prisma.chat_messages.create({
+        data: {
+          sender_id: payload.sender_id,
+          receiver_id: payload.receiver_id,
+          channel_id: payload.channel_id,
+          content: payload.content,
+        },
+      });
+      this.logger.log(`Received ${JSON.stringify(payload)}`);
+      this.server.emit('msgToClient', newMessage, client.id);
+    } catch (error) {
+      console.error('Error creating message:', error);
+    }
   }
 
   afterInit(server: Server) {
-    this.logger.log('Init');
+    this.logger.log(`Init: ${server}`);
   }
 
   handleConnection(client: Socket) {
-    this.logger.log('Client connected: ${client.id}');
+    this.logger.log(`Client connected: ${client.id}`);
   }
 
   handleDisconnect(client: Socket) {
-    this.logger.log('Client disconnected: ${client.id}');
+    this.logger.log(`Client disconnected: ${client.id}`);
   }
 }
