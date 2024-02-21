eba9fa1967ee9edff4beeba8d16475f8416a9115
cpereira42 <cpereira@student.42sp.org.br>
2024-02-21 01:09:51 -0300

    solved bugs friends

--- back/src/friends/friends.service.ts
+++ back/src/friends/friends.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { FriendsRepository } from './repositories/friends.repository';
 import { friends } from '@prisma/client';
 import { UsersService } from 'src/users/users.service';
@@ -12,6 +17,14 @@ export class FriendsService {
   async addFriend(userId: any, friendId: number): Promise<friends> {
     const friendExists = await this.userService.findOne(friendId);
     if (friendExists) {
+      if (friendExists.user_id == userId) {
+        throw new BadRequestException('You can not add yourself');
+      }
+      const checkFriend = await this.userService.findFriends(userId);
+      if (checkFriend.find(friend => friend.user_id === friendId)) {
+        throw new ConflictException('You are already friends.');
+      }
+
       return this.repository.addFriend(userId, friendId);
     }
     throw new NotFoundException('User not found.');
